62526fb92e55ef714fb6cd68eaa17190955b5310
antoniamescia <antonia.mescia.novarese@gmail.com>
2021-04-26 22:20:42 -0300

    Ej 1 - Parte 1

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -26,3 +26,11 @@ namespace Expert_SRP
         }
     }
 }
+
+/*
+La clase Kiosko no cumple con el patrón Expert. Una de sus responsabilidades es calcular el precio del alfajor, pero esta clase no es la experta en información pues se la pide a Alfajor.
+El precio del alfajor debería ser calculado en la clase Alfajor, que sí es la experta en información.
+
+Tampoco cumple con el principio SRP porque encontramos más de una razón por la cual la clase puede cambiar: podría cambiar la condición para poder comprar el alfajor, y podría cambiar el cálculo del cambio de moneda (si introducimos otra moneda diferente que no sean U$S).
+
+*/
