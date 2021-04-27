01c03e2eb378c46e6dd3909314ea984f1dc57029
Luciana Diaz <ldiaz2511@gmail.com>
2021-04-27 11:45:19 -0300

    Modificacion para la parte 2

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -11,12 +11,13 @@ namespace Expert_SRP
         }
         public Double PrecioDulce {get;set;}
         public Double PrecioMasa {get; set;}
+
+        public double PrecioAlfajor(double PrecioMasa, double PrecioDulce)
+        {
+           return PrecioDulce + PrecioMasa;
+        }
+
+
     }
 }
-/*
-La clase Alfajor cumple con el principio SRP. Esta responsabilidad está totalmente encapsulada por la clase por las propiedades get y set, y no hallamos más de una razón por la cual la clase podría cambiar.
 
-Como mencionamos anteriormente, la responsabilidad de esta clase es conocer el precio de los ingredientes. Si la miramos indpendientemente de la clase Kiosko, cumple con el patrón Expert porque ninguna otra clase conoce los precios. 
-Si se analiza en conjunto a la clase Kiosko, concluímos que no cumple con el patrón Expert porque es quien debería calcular el precio de los alfajores (por ser el experto en información), pero no lo hace.
-
-*/
